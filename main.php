5da46ffb95c114277b6b20eef1db7a5893f5554c
shalbayev <56976466+shalbayev@users.noreply.github.com>
2019-12-23 23:01:17 +0600

    Add files via upload

---
+++ main.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>To Do</title>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <header>
+    <div id="logo" >
+      <span>To DO</span>
+    </div>
+    <div id="about">
+      <a href="#" title="Возможности" >Home</a>
+      <a href="#" title="Преимущества">Features</a>
+      <a href="#" title="Контакты">Contacts</a>
+      <a href="#" title="Ответы на вопросы">About us</a>
+    </div>
+  </header>
+
+  <div id="top">
+    <h1>to DO list</h1>
+    <h3>create your own day!</h3>
+    <img src="images/get-it-on-google-play.svg" alt="" id="pm">
+    <img src="images/available-on-the-app-store.svg" alt="" id="app">
+  </div>
+
+  <div id="main">
+    <div class="intro">
+      <h2>Why do we need it?</h2>
+      <span>Большой выбор всего, что может вам пригодиться</span>
+    </div>
+    <div class="text">
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos impedit, ut vero commodi voluptate? Ipsam reiciendis, necessitatibus incidunt ipsa quis sapiente excepturi, optio iusto voluptatibus dolorem officia quidem, numquam eaque minus, autem praesentium rem architecto dolores. Ipsa unde id velit optio magni enim! Illum obcaecati, dolorem distinctio nemo, ipsum iure.</span>
+    </div>
+  </div>
+ 
+  <div id="contacts">
+  	
+    <center><h5>Sign in</h5></center>
+    <form id="form_input" action="validation-form/auth.php" method="post">
+      <input type="text" placeholder="Login" name="login" id="login"><br>
+			<input type="password" placeholder="Password" name="pass" id="password"><br>
+			
+						<button class="btn " type="submit">Sign In</button>
+      <a href="signup.html" id="register">If you don't have an account,sign up</a>
+    </form>
+  </div>
+  
+  
+  <div id="faq">
+    <div>
+      <span class="title">Lorem ipsum dolor sit amet</span><br>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+    </div>
+    <div>
+      <span class="title">Lorem ipsum dolor sit amet</span><br>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+    </div>
+    <div>
+      <span class="title">Lorem ipsum dolor sit amet</span><br>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+      <span class="heading">Lorem ipsum dolor sit amet</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae asperiores debitis perspiciatis perferendis nemo tempore distinctio officia commodi et non tempora laudantium culpa nostrum, quidem, quasi ratione itaque nam.</p>
+    </div>
+  </div>
+
+ 
+</body>
+</html>
